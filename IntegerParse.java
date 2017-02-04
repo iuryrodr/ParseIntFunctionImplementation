66170b3e42036e645e89476576cfd974d755a40f
Ashwin Radhakrishnan <Ashwin@Ashwins-MacBook-Pro.local>
2017-02-04 02:04:53 -0800

    Initial commit

---
+++ IntegerParse.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Ashwin on 2/4/17.
+ */
+public class IntegerParse {
+    public static void parseInteger(String string) {
+        if(string.length() == 0 || !string.matches("\\d+")) {
+            System.out.println("Invalid String");
+            return;
+        }
+
+        System.out.println(string.substring(0));
+    }
+
+    public static void main(String a[]) {
+        parseInteger("239");
+    }
+}
